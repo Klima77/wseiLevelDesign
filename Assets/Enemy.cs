1395cac99e4e60d85cdece8988f0c8b9bffef9f1
Klima77 <kacper.klimczak@microsoft.wsei.edu.pl>
2025-05-05 19:03:03 +0200

    dziala strzelanie do zlonierzy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //Check if the colliding object is a bullet
+    //    if (collision.gameObject.CompareTag("Bullet"))
+    //    {
+    //        Destroy(gameObject); //Destroy the enemy
+    //    }
+    //}
+}
