f7e190a40ec06470f288a6e7d4bc9390985af9f3
Klima77 <kacper.klimczak@microsoft.wsei.edu.pl>
2025-05-13 00:44:27 +0200

    Jeszcze kilka fixow

--- Assets/SciFi_Door/Script/door.cs
+++ Assets/SciFi_Door/Script/door.cs
@@ -5,12 +5,13 @@ public class door : MonoBehaviour {
 	GameObject thedoor;
 
 void OnTriggerEnter ( Collider obj  ){
-	thedoor= GameObject.FindWithTag("SF_Door");
-	thedoor.GetComponent<Animation>().Play("close");
+	thedoor= GameObject.FindWithTag("LastDoors");
+	thedoor.GetComponent<Animation>().Play("open");
 }
 
-//void OnTriggerExit ( Collider obj  ){
-//	thedoor= GameObject.FindWithTag("SF_Door");
-//	thedoor.GetComponent<Animation>().Play("open");
-//}
+
+    void OnTriggerExit ( Collider obj  ){
+    	thedoor= GameObject.FindWithTag("LastDoors");
+    	thedoor.GetComponent<Animation>().Play("close");
+    }
 }
